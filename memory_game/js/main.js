da0099ce7a638c2d2334cbdc90a89258f32b2888
Winston Quin <winston.quin@iqagency.com>
2017-10-26 16:15:50 -0400

    create game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,20 +1,19 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-var cardThree = cards[2];
-var cardFour = cards[3];
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardThree);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
-
-
-if (cardsInPlay.length === 2) {
-  if (cardsInPlay[0] === cardsInPlay[1]) {
-    alert("You found a match!");
-  } else {
-    alert("Sorry, try again.");
+var checkForMatch = function() {
+  if (cardsInPlay.length === 2) {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+      alert("You found a match!");
+    } else {
+      alert("Sorry, try again.");
+    }
   }
 }
+
+var flipCard = function (cardId) {
+  console.log("User flipped " + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+  checkForMatch();
+}
+flipCard(0);
+flipCard(2);
