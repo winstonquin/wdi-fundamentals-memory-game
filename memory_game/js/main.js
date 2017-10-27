e2590444140f0b56ca8ddee015229bb87a7354b7
Winston Quin <winston.quin@iqagency.com>
2017-10-27 15:55:48 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "../images/queen-of-hearts.png",
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "../images/queen-of-diamonds.png",
+  },
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "../images/king-of-hearts.png",
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "../images/king-of-diamonds.png",
+  }
+];
 var cardsInPlay = [];
 var checkForMatch = function() {
   if (cardsInPlay.length === 2) {
@@ -11,8 +32,8 @@ var checkForMatch = function() {
 }
 
 var flipCard = function (cardId) {
-  console.log("User flipped " + cards[cardId]);
-  cardsInPlay.push(cards[cardId]);
+  console.log("User flipped " + cards[cardId].rank + " " + cards[cardId].suit + " " + cards[cardId].cardImage);
+  cardsInPlay.push(cards[cardId].rank);
   checkForMatch();
 }
 flipCard(0);
